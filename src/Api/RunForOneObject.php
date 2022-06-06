8de4f22ad564c6a82ba8b892880fe62a986dd6de
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-06-06 23:54:24 +1200

    WIP

--- src/Api/RunForOneObject.php
+++ src/Api/RunForOneObject.php
@@ -82,20 +82,20 @@ class RunForOneObject
     {
         $this->object = $object;
         $this->verbose = $verbose;
-        // if (false === $this->hasStages()) {
-        //     if($this->verbose) {
-        //         DB::alteration_message('... ... ... Error, no stages', 'deleted');
-        //     }
-        //     return 0;
-        // }
-        //
-        // echo 'B';
-        // if (false === $this->object->isLiveVersion()) {
-        //     if($this->verbose) {
-        //         DB::alteration_message('... ... ... Error, not a live version', 'deleted');
-        //     }
-        //     return 0;
-        // }
+        if (false === $this->hasStages()) {
+            if($this->verbose) {
+                DB::alteration_message('... ... ... Error, no stages', 'deleted');
+            }
+            return 0;
+        }
+
+        echo 'B';
+        if (false === $this->object->isLiveVersion()) {
+            if($this->verbose) {
+                DB::alteration_message('... ... ... Error, not a live version', 'deleted');
+            }
+            return 0;
+        }
 
         // array of version IDs to delete
         // IMPORTANT
